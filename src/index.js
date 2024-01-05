ebbbd8a591bf225018dd0d88be3ead8b6006a3b6
hollyallison <145831646+hollyallison@users.noreply.github.com>
2024-01-05 13:59:28 +0000

    updated css

--- src/index.js
+++ src/index.js
@@ -14,12 +14,12 @@ function generateRecipe(event) {
     let instructionsInput = document.querySelector('#user-instructions');
     let apiKey = "2c40a31a9bebb30oc02aftf7a42a8b2e";
     let prompt = `User instructions: Generate a ${instructionsInput.value} recipe`;
-    let context = "Follow user instructions and display the answer in html format where title of the recipe is included in class element named title, ingredients and quantities in class element named ingredients using list format <ul><li></li><li></li><li></li><li><li> <li></li></ul>. Put cooking instructions in class element named method with numbers for each step. Please always start part 3 with Cooking Method in the top line. Please RESPECT the css for EACH class and keep this css consistent";
+    let context = "Follow user instructions and display the answer in html format where title of the recipe is included in class element named title, ingredients and quantities in class element named ingredients using list format <ul><li></li><li></li><li></li><li><li> <li></li></ul> using the heading ingredients. Put cooking instructions in class element named method with numbers for each step. Please always start class element method with Cooking Method in the top line. Please RESPECT the css for EACH class and keep this css consistent";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     let recipeElement = document.querySelector("#recipe");
     recipeElement.classList.remove("hidden");
-    recipeElement.innerHTML = `⏳Generating a ${instructionsInput.value} recipe for you`;
+    recipeElement.innerHTML = `⏳  Generating a ${instructionsInput.value} recipe for you`;
     axios.get(apiUrl).then(displayRecipe).catch(error => console.error('Error fetching recipe:', error));
 
     console.log("Generating recipe");
